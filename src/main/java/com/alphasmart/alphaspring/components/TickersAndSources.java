663fda2844b59990b50b9635f5f5f4a46c7cc9a8
Mao <maoyuan@gmail.com>
2019-11-23 21:28:25 -0500

    updates twitter source

--- src/main/java/com/alphasmart/alphaspring/components/TickersAndSources.java
+++ src/main/java/com/alphasmart/alphaspring/components/TickersAndSources.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class TickersAndSources {
-    public static ArrayList<String> getTickers(){
+    public static ArrayList<String> getTickers() {
         return new ArrayList<String>(Arrays.asList("ABN", "ADYEN", "INGA", "KPN", "RDSA", "BNP"));
     }
 
-    public static ArrayList<String> getSources(){
-        return new ArrayList<String>(Arrays.asList("euronext", "wsj", "ft", "business", "zerohedge", "cnbc"));
+    public static ArrayList<String> getSources() {
+        return new ArrayList<String>(Arrays.asList("marketwatch", "wsj", "ft", "business", "theeconomist", "cnbc", "barronsonline"));
     }
 }
