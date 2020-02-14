0deffc0a2109aa8ed14b1b28aa21d6047ac72249
mao <maoyuan@gmail.com>
2020-02-14 18:23:21 -0500

    datetime mockito test ok

--- src/test/java/com/alphasmart/alphaspring/components/StockQuotesTest.java
+++ src/test/java/com/alphasmart/alphaspring/components/StockQuotesTest.java
@@ -3,17 +3,19 @@ package com.alphasmart.alphaspring.components;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class StockQuotesTest {
     StockQuotes stockQuotes;
 
     @BeforeEach
     public void setup(){
-        stockQuotes = new StockQuotes();
+        stockQuotes = spy(new StockQuotes());
     }
 
     @Test
@@ -26,7 +28,10 @@ class StockQuotesTest {
 
     @Test
     void startAndEndDate() {
-        List<String> expectBeginEndDate = Arrays.asList("2019-02-14","2020-02-14");
-        assertEquals(expectBeginEndDate,StockQuotes.startAndEndDate(1));
+        List<String> expectBeginEndDate = Arrays.asList("2019-03-01","2020-03-01");
+        LocalDate fakeToday = LocalDate.of(2020,3,1);
+        doReturn(fakeToday).when(stockQuotes).getToday();
+        assertEquals(expectBeginEndDate,stockQuotes.startAndEndDate(1));
+
     }
 }
