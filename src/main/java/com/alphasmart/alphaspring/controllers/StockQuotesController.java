fc5576a2c7b2ffc9012584ae62dc7783068bccd7
mao <maoyuan@gmail.com>
2020-02-24 19:09:41 -0500

    switch from RequestMapping to GetMapping

--- src/main/java/com/alphasmart/alphaspring/controllers/StockQuotesController.java
+++ src/main/java/com/alphasmart/alphaspring/controllers/StockQuotesController.java
@@ -2,10 +2,7 @@ package com.alphasmart.alphaspring.controllers;
 
 import com.alphasmart.alphaspring.components.StockQuotes;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -14,7 +11,7 @@ public class StockQuotesController {
     @Autowired
     StockQuotes stockQuotes;
 
-    @RequestMapping(method = RequestMethod.GET, path = "api/quote")
+    @GetMapping("api/quote")
     public String featuredStocksQuote() {
         return stockQuotes.getQuote();
     }
