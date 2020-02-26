c181978a60eb0cf9f4b1f21e43492ba0b4653e25
mao <maoyuan@gmail.com>
2020-02-26 16:44:22 -0500

    cleans up FeedbackRepositoryIntegrationTest

--- src/test/java/com/alphasmart/alphaspring/models/FeedbackRepositoryIntegrationTest.java
+++ src/test/java/com/alphasmart/alphaspring/models/FeedbackRepositoryIntegrationTest.java
@@ -1,23 +1,20 @@
 package com.alphasmart.alphaspring.models;
 
-import com.alphasmart.alphaspring.AlphaspringApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
 
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ContextConfiguration(classes = AlphaspringApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
-@DataJpaTest
 public class FeedbackRepositoryIntegrationTest {
+
     @Autowired
     FeedbackRepository feedbackRepository;
 
@@ -25,8 +22,7 @@ public class FeedbackRepositoryIntegrationTest {
     public void ifNewFeedbackSaved_thenSuccess(){
         Feedback feedback = new Feedback("John Doe","jd@gmail.com","405-909-2934","good job");
         feedbackRepository.save(feedback);
-        List<Feedback> feedbacks = (List<Feedback>) feedbackRepository.findAll();
-        assertEquals(4,feedbacks.size());
+        assertEquals(4,((List<Feedback>) feedbackRepository.findAll()).size());
         Optional<List<Feedback>> jennyFeedbacks = feedbackRepository
                 .findFeedbacksByEmail("Jenny.H@bell.ca");
         assertEquals("Jenny",jennyFeedbacks.get().get(0).getName());
