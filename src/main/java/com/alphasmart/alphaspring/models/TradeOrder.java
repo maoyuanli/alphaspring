2b5d75f27e5b27c0598cc91f6d32ebff16c063bd
mao <maoyuan@gmail.com>
2020-02-26 14:47:07 -0500

    initialization of dummy TradeOrder data

--- src/main/java/com/alphasmart/alphaspring/models/TradeOrder.java
+++ src/main/java/com/alphasmart/alphaspring/models/TradeOrder.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class TradeOrder {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String ticker;
     private String orderType;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,13 +2,13 @@ INSERT INTO trade_account (account_no) VALUES ('TA001');
 INSERT INTO trade_account (account_no) VALUES ('TA002');
 INSERT INTO trade_account (account_no) VALUES ('TA003');
 
--- INSERT INTO trade_order (created_date, order_price, order_type, order_volumn,ticker,trade_account_id)
--- VALUES (current_timestamp,'15.05','Limited Buy','3500','ABN AMRO BANK N.V. (ABN)',1);
--- INSERT INTO trade_order (created_date, order_price, order_type, order_volumn,ticker,trade_account_id)
--- VALUES (current_timestamp,null,'Market Sell','20000','ROYAL DUTCH SHELLA (RDSA)',1);
--- INSERT INTO trade_order (created_date, order_price, order_type, order_volumn,ticker,trade_account_id)
--- VALUES (current_timestamp,'15.10','Limited Buy','7000','ABN AMRO BANK N.V. (ABN)',2);
---
+INSERT INTO trade_order (created_date, order_price, order_type, order_volumn,ticker)
+VALUES (current_timestamp,'15.05','Limited Buy','3500','ABN AMRO BANK N.V. (ABN)');
+INSERT INTO trade_order (created_date, order_price, order_type, order_volumn,ticker)
+VALUES (current_timestamp,null,'Market Sell','20000','ROYAL DUTCH SHELLA (RDSA)');
+INSERT INTO trade_order (created_date, order_price, order_type, order_volumn,ticker)
+VALUES (current_timestamp,'15.10','Limited Buy','7000','ABN AMRO BANK N.V. (ABN)');
+
 INSERT INTO feedback(comment, created_date, email, name, phone)
 VALUES ('hi, good job!',current_timestamp,'shneider.gunn@gmail.com','Shneider, Gunn','123-098-9383');
 INSERT INTO feedback(comment, created_date, email, name, phone)
