fe14a63e864be49fb8e5e01be894c50c0df44997
mao <maoyuan@gmail.com>
2020-02-26 21:42:15 -0500

    utilizes AOP and Logging

--- src/main/java/com/alphasmart/alphaspring/logging/ApplicationLoggerAspect.java
+++ src/main/java/com/alphasmart/alphaspring/logging/ApplicationLoggerAspect.java
@@ -3,10 +3,8 @@ package com.alphasmart.alphaspring.logging;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,28 +14,31 @@ public class ApplicationLoggerAspect {
     private final Logger LOGGER = LogManager.getLogger(this.getClass());
 
     @Pointcut("within(com.alphasmart.alphaspring.components..*)")
-    public void componentPoincut(){}
+    public void componentPoincut() {
+    }
 
     @Before("componentPoincut()")
-    public void logComponentBefore(JoinPoint joinPoint){
-        LOGGER.debug(String.format("-*-*-*-*-*-*-*-*-*-*-* Before Component [ %s ] Run *-*-*-*-*-*-*-*-*-*-*-*-*-",joinPoint.getSignature().getDeclaringTypeName()));
+    public void logComponentBefore(JoinPoint joinPoint) {
+        LOGGER.debug(String.format("-*-*-*-*-*-*-*-*-*-*-* Before Component [ %s ] Run *-*-*-*-*-*-*-*-*-*-*-*-*-", joinPoint.getSignature().getDeclaringTypeName()));
     }
 
     @After("componentPoincut()")
-    public void logComponentAfter(JoinPoint joinPoint){
-        LOGGER.debug(String.format("-*-*-*-*-*-*-*-*-*-*-* After Component [ %s ] Run *-*-*-*-*-*-*-*-*-*-*-*-*-",joinPoint.getSignature().getDeclaringTypeName()));
+    public void logComponentAfter(JoinPoint joinPoint) {
+        LOGGER.debug(String.format("-*-*-*-*-*-*-*-*-*-*-* After Component [ %s ] Run *-*-*-*-*-*-*-*-*-*-*-*-*-", joinPoint.getSignature().getDeclaringTypeName()));
     }
 
     @Pointcut("within(com.alphasmart.alphaspring.services..*)")
-    public void servicePoincut(){}
-
-    @Before("servicePoincut()")
-    public void logServiceBefore(JoinPoint joinPoint){
-        LOGGER.debug(String.format("-*-*-*-*-*-*-*-*-*-*-* Before Service [ %s ] Run *-*-*-*-*-*-*-*-*-*-*-*-*-",joinPoint.getSignature().getDeclaringTypeName()));
+    public void servicePoincut() {
     }
 
-    @After("servicePoincut()")
-    public void logServiceAfter(JoinPoint joinPoint){
-        LOGGER.debug(String.format("-*-*-*-*-*-*-*-*-*-*-* After Service [ %s ] Run *-*-*-*-*-*-*-*-*-*-*-*-*-",joinPoint.getSignature().getDeclaringTypeName()));
+    @Around("servicePoincut()")
+    public Object logServiceAround(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        LOGGER.debug(String.format("-*-*-*-*-*-*-*-*-*-*-* Before Service [ %s ] Run *-*-*-*-*-*-*-*-*-*-*-*-*-", joinPoint.getSignature().getDeclaringTypeName()));
+        Object o = joinPoint.proceed();
+        LOGGER.debug(String.format("-*-*-*-*-*-*-*-*-*-*-* After Service [ %s ] Run *-*-*-*-*-*-*-*-*-*-*-*-*-", joinPoint.getSignature().getDeclaringTypeName()));
+
+        return o;
     }
 }
+
