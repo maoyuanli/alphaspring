0857b40aeba0fae5f17de4d82017d0b9d26acaf8
mao <maoyuan@gmail.com>
2020-02-27 13:41:14 -0500

    SecurityConfig preliminary setup

--- src/main/java/com/alphasmart/alphaspring/security/SecurityConfig.java
+++ src/main/java/com/alphasmart/alphaspring/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
-                .antMatchers("/**").authenticated().and().httpBasic();
+                .antMatchers("/**").permitAll();
 
 
         http.cors().and().csrf().disable();
